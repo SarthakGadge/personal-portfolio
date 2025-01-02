925537e0938a7dd7764b288ec40f47fcfdcd1d50
SarthakGadge <sarthakgadge43@gmail.com>
2025-01-02 22:26:30 +0530

    Initial Commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="flex justify-center text-2xl font-bold text-blue-600">
+      Tailwind CSS is working!
+    </div>
+  );
+}
